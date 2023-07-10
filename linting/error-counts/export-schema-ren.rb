a400808e75bf571b3dfd086660f9b90f0f2d5086
Ren Chaturvedi <ren.chaturvedi@shopify.com>
2023-07-10 16:51:55 -0400

    make some changes that didn't work

--- linting/error-counts/export-schema-ren.rb
+++ linting/error-counts/export-schema-ren.rb
@@ -22,7 +22,7 @@ schema.types.each_value do |type|
   # Count and save description for the type itself
   if type.description
     type_count += 1
-    descriptions << ["Type", type.name, type.description, "Type: #{type.name}"]
+    descriptions << ["Type", type.name, type.description, "Type: #{type.name}", "N/A"]
   end
 
   # If the type has fields, iterate over them
@@ -31,7 +31,7 @@ schema.types.each_value do |type|
       # Count and save description for the field
       if field.description
         field_count += 1
-        descriptions << ["Field", field.name, field.description, "Type: #{type.name}, Field: #{field.name}"]
+        descriptions << ["Field", field.name, field.description, "Type: #{type.name}, Field: #{field.name}", field.type.to_s]
       end
 
       # If the field has arguments, iterate over them
@@ -39,7 +39,7 @@ schema.types.each_value do |type|
         # Count and save description for the argument
         if argument.description
           argument_count += 1
-          descriptions << ["Argument", argument.name, argument.description, "Type: #{type.name}, Field: #{field.name}, Argument: #{argument.name}"]
+          descriptions << ["Argument", argument.name, argument.description, "Type: #{type.name}, Field: #{field.name}, Argument: #{argument.name}", argument.type.to_s]
         end
       end
     end
@@ -51,7 +51,7 @@ schema.types.each_value do |type|
       # Count and save description for the enum value
       if enum_value.description
         enum_value_count += 1
-        descriptions << ["Enum Value", enum_value.graphql_name, enum_value.description, "Type: #{type.name}, Enum Value: #{enum_value.graphql_name}"]
+        descriptions << ["Enum Value", enum_value.graphql_name, enum_value.description, "Type: #{type.name}, Enum Value: #{enum_value.graphql_name}", "N/A"]
       end
     end
   end
@@ -59,19 +59,19 @@ end
 
 # Create a table
 table = Terminal::Table.new do |t|
-  t << ['Element', 'Count']
+  t << ['Element', 'Count', 'Data Type']
   t << :separator
-  t << ['Type', type_count]
-  t << ['Field', field_count]
-  t << ['Argument', argument_count]
-  t << ['Enum Value', enum_value_count]
+  t << ['Type', type_count, 'N/A']
+  t << ['Field', field_count, 'N/A']
+  t << ['Argument', argument_count, 'N/A']
+  t << ['Enum Value', enum_value_count, 'N/A']
 end
 
 puts table
 
 # Generate a CSV file with all the descriptions
 CSV.open("descriptions.csv", "wb") do |csv|
-  csv << ["Element Type", "Element Name", "Description", "Identifier"]
+  csv << ["Element Type", "Element Name", "Description", "Identifier", "Data Type"]
   descriptions.each do |description|
     csv << description
   end
